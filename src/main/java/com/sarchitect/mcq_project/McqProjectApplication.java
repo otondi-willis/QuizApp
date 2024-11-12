939fb286c0fa690a745e539d88335337648cc492
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-12 04:31:05 +0300

    Initial Commit

---
+++ src/main/java/com/sarchitect/mcq_project/McqProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sarchitect.mcq_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McqProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarchitect/mcq_project/McqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarchitect.mcq_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McqProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
