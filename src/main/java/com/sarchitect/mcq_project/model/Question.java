911cd5797edf7f9c7ee1a967d651587ef082a9f0
otondi-willis <willis.otondi.ke@gmail.com>
2024-11-13 04:47:01 +0300

    Created Question class

---
+++ src/main/java/com/sarchitect/mcq_project/model/Question.java
@@ -0,0 +1,13 @@
+package com.sarchitect.mcq_project.model;
+
+public class Question {
+    private int id;
+    private String questionTitle;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String rightAnswer;
+    private String difficulty;
+    private String category;
+}
